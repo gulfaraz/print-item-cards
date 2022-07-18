0add1f9d757ba578a043660e375ecba9aff91377
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2022-07-18 16:00:01 +0200

    feat: fetch from notion to json then create pdf from json

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,149 @@
 import { Client } from "@notionhq/client";
 import dotenv from "dotenv";
+import fs from "fs";
 
 dotenv.config();
+
+const getProperty = async (notion: Client, page: any, propertyName: string) => {
+  const propertyId = page.properties[propertyName].id;
+
+  const property = (await notion.pages.properties.retrieve({
+    page_id: page.id,
+    property_id: propertyId,
+  })) as any;
+
+  let propertyValue = property[property.type];
+
+  if ("Rarity" == propertyName || "Type" == propertyName) {
+    if (property[property.type] && "name" in property[property.type])
+      propertyValue = property[property.type].name;
+  } else if (
+    "Attunement" == propertyName ||
+    "Description" == propertyName ||
+    "Description (Identified)" == propertyName ||
+    "Description (Exalted)" == propertyName
+  ) {
+    if (property.results.length > 0)
+      propertyValue = property.results[0].rich_text.text.content;
+    else propertyValue = null;
+  } else if ("Name" == propertyName) {
+    propertyValue = property.results[0].title.text.content;
+  } else if ("Number" == propertyName) {
+    propertyValue = String(propertyValue).padStart(6, "0");
+  }
+
+  return propertyValue;
+};
+
+const getItemCardValues = async (notion: Client, itemPageId: any) => {
+  const itemPage = await notion.pages.retrieve({
+    page_id: itemPageId,
+  });
+
+  const number = await getProperty(notion, itemPage, "Number");
+  const name = await getProperty(notion, itemPage, "Name");
+  const attunement = await getProperty(notion, itemPage, "Attunement");
+  const type = await getProperty(notion, itemPage, "Type");
+  const rarity = await getProperty(notion, itemPage, "Rarity");
+  const description = await getProperty(notion, itemPage, "Description");
+  const descriptionIdentified = await getProperty(
+    notion,
+    itemPage,
+    "Description (Identified)"
+  );
+  const descriptionExalted = await getProperty(
+    notion,
+    itemPage,
+    "Description (Exalted)"
+  );
+
+  const itemCards = [
+    {
+      number,
+      name,
+      attunement,
+      type,
+      rarity,
+      description,
+    },
+  ];
+
+  if (descriptionIdentified) {
+    const itemCardIdentified = { ...itemCards[0] };
+    itemCardIdentified.description += "\n\n" + descriptionIdentified;
+    itemCards.push(itemCardIdentified);
+  }
+
+  if (descriptionExalted) {
+    const itemCardExalted = { ...itemCards[0] };
+    itemCardExalted.name = itemCardExalted.name + " (Exalted)";
+    if (descriptionIdentified) {
+      itemCardExalted.description += "\n\n" + descriptionIdentified;
+    }
+    itemCardExalted.description += "\n\n" + descriptionExalted;
+    itemCards.push(itemCardExalted);
+  }
+
+  return itemCards;
+};
+
+const getItemCards = async (notion: Client, page: any) => {
+  const itemId = page.properties.Items.id;
+
+  const itemPages = (
+    (await notion.pages.properties.retrieve({
+      page_id: page.id,
+      property_id: itemId,
+    })) as any
+  ).results;
+
+  const itemCards = await Promise.all(
+    itemPages.map(
+      async (itemPage: any) =>
+        await getItemCardValues(notion, itemPage.relation.id)
+    )
+  );
+
+  return itemCards;
+};
+
+const getItems = async (notion: Client, itemPages: any[]) => {
+  const items = [] as any[];
+  for (const itemPageIndex in itemPages) {
+    const itemPage = itemPages[itemPageIndex];
+    const itemCardCount = await getProperty(notion, itemPage, "Count");
+    const itemCards = await getItemCards(notion, itemPage);
+    const itemCardsIncludingDuplicates = itemCards.reduce(
+      (res, current) => res.concat(...Array(itemCardCount).fill(current)),
+      []
+    );
+
+    items.push(...itemCardsIncludingDuplicates);
+  }
+
+  return items;
+};
 
 async function main() {
   const notion = new Client({
     auth: process.env.NOTION_TOKEN,
   });
 
-  const response = await notion.databases.query({
-    database_id: "FIXME",
-  });
+  const items = (
+    await notion.databases.query({
+      database_id: process.env.DATABASE_ID!,
+      filter: { property: "Print", checkbox: { equals: true } },
+    })
+  ).results;
 
-  console.log("Got response:", response);
+  const itemCards = await getItems(notion, items);
+
+  const filename = `item_cards_${new Date().toJSON().slice(0, 10)}.json`;
+
+  fs.writeFile(filename, JSON.stringify(itemCards, null, 4), (error) => {
+    if (error) throw error;
+    console.log(`${itemCards.length} item cards saved in ${filename}`);
+  });
 }
 
-main()
-  .then(() => process.exit(0))
-  .catch((err) => {
-    console.error(err);
-    process.exit(1);
-  });
+main();
---
+++ src/pdf.ts
@@ -0,0 +1,112 @@
+import fs from "fs";
+
+const pdf: any = require("pdf-creator-node");
+
+function getCardSize(card: any) {
+  if (!("cardSize" in card)) {
+    card.cardSize = calculateCardSize(card);
+  }
+  return card.cardSize;
+}
+
+function calculateCardSize(card: any) {
+  let cardSize = 0;
+  const newLinesWeight = 50;
+  const newLinesCount = (card.description.match(/\n\n/g) || []).length;
+  const descriptionLength =
+    card.description.length +
+    (newLinesCount + (card.attunement && card.attunement.length > 0 ? 2 : 0)) *
+      newLinesWeight;
+  if (descriptionLength > 650) {
+    cardSize = 2;
+  } else if (descriptionLength > 300) {
+    cardSize = 1;
+  }
+  return cardSize;
+}
+
+function sortCards(cards: any[]) {
+  const sortedCards = cards.sort((a, b) => getCardSize(b) - getCardSize(a));
+  return sortedCards;
+}
+
+function batchCards(cards: any[]) {
+  const largeCards = cards.filter((card) => card.cardSize == 2);
+  const mediumCards = cards.filter((card) => card.cardSize == 1);
+  const smallCards = cards.filter((card) => card.cardSize == 0);
+  const batchedCards = [];
+  while (largeCards.length > 0 && smallCards.length > 0) {
+    batchedCards.push([largeCards.pop(), smallCards.pop()]);
+  }
+  while (mediumCards.length > 1) {
+    batchedCards.push([mediumCards.pop(), mediumCards.pop()]);
+  }
+  while (mediumCards.length > 0 && smallCards.length > 1) {
+    batchedCards.push([mediumCards.pop(), smallCards.pop(), smallCards.pop()]);
+  }
+  while (smallCards.length > 3) {
+    batchedCards.push([
+      smallCards.pop(),
+      smallCards.pop(),
+      smallCards.pop(),
+      smallCards.pop(),
+    ]);
+  }
+
+  while (largeCards.length > 0) {
+    batchedCards.push([largeCards.pop()]);
+  }
+  while (mediumCards.length > 0) {
+    batchedCards.push([mediumCards.pop()]);
+  }
+  while (smallCards.length > 0) {
+    batchedCards.push([smallCards.pop()]);
+  }
+  return batchedCards;
+}
+
+function createPdf(filename: string, cards: any[]) {
+  const html = fs.readFileSync("item_card_template.html", "utf8");
+
+  const options = {
+    format: "A4",
+    orientation: "landscape",
+    border: "0",
+    header: { height: "0", contents: null },
+    footer: { height: "0", contents: null },
+  };
+
+  const document = {
+    html: html,
+    data: { batches: cards },
+    path: `./${filename}.pdf`,
+    type: "pdf",
+  };
+
+  pdf
+    .create(document, options)
+    .then((res: any) => {
+      console.log(res);
+    })
+    .catch((error: Error) => {
+      console.error(error);
+    });
+}
+
+async function main() {
+  if (process.argv.length > 2) {
+    const filename = process.argv[2];
+
+    fs.readFile(filename, "utf8", function (err, data) {
+      if (err) throw err;
+      const cards = JSON.parse(data);
+      const sortedCards = sortCards(cards);
+      const batchedCards = batchCards(sortedCards);
+      createPdf(filename.replace(/\.[^/.]+$/, ""), batchedCards);
+    });
+  } else {
+    throw new Error("filename required");
+  }
+}
+
+main();
